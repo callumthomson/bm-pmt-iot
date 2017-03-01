56d3914a969595a658591c929c34bc901c83b402
waltoncon <waltoncon@gmail.com>
2017-03-01 09:46:35 -0500

    Changed background from red to green

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #f00;
+                background-color: #0f0;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
