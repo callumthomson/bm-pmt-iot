5d788046cf82b41d27abb40236bcb588a3201a62
Callum Thomson <callum@callumthomson.co.uk>
2017-03-02 13:24:37 +0000

    Return devices from devices route

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Device;
+
 class DeviceController extends Controller
 {
-    //
+    public function index()
+    {
+        $devices = Device::all();
+        return $devices;
+        //return view('device.index', ['devices' => $devices]);
+    }
 }
