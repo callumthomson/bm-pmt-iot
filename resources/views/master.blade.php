7b7cef24bc929a12cf5b25ab7d3d0752e90cadb4
Callum Thomson <callum@callumthomson.co.uk>
2017-03-03 15:12:51 +0000

    Master bootstrap template

---
+++ resources/views/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        @yield('head-start')
+        <title>{{ $title or 'Default' }}</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/style.css">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        @yield('head-end')
+    </head>
+    <body>
+        @yield('body')
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        @yield('scripts')
+    </body>
+</html>
