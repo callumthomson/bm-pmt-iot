83626318a5c8e837ca3dcc54e0bb9f7417ac049e
Callum Thomson <callum@callumthomson.co.uk>
2017-03-03 16:44:25 +0000

    Show devices on device page

--- app/Device.php
+++ app/Device.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
@@ -25,5 +26,24 @@ class Device extends Model
     public function device_type()
     {
         return $this->hasOne('App\DeviceType', 'id', 'id');
+    }
+
+    /**
+     * GETTER
+     * Get the last message transmitted by the device
+     *
+     * @return Message|null
+     */
+    public function getLastMessageAttribute()
+    {
+        $message = Message::where('device_id', '=', $this->attributes['id'])
+            ->orderBy('created_at', 'desc')
+            ->first();
+        if($message) {
+            return $message;
+        } else {
+            $message = new Message;
+            $message->created_at = Carbon::now();
+        }
     }
 }
--- resources/views/device/index.blade.php
+++ resources/views/device/index.blade.php
@@ -2,15 +2,22 @@
 
 @section('body')
     <div class="container">
+        <h1>Devices</h1>
         <table class="table">
             <tr>
                 <th>Name</th>
                 <th>Type</th>
+                <th>Last Communication Received</th>
             </tr>
             @foreach($devices as $device)
                 <tr>
-                    <td>{{$device->name }}</td>
+                    <td>{{ $device->name }}</td>
                     <td>{{ $device->device_type->name }}</td>
+                    @if($device->last_message)
+                    <td>{{ $device->last_message->created_at->diffForHumans() }}</td>
+                    @else
+                    <td>Never</td>
+                    @endif
                 </tr>
             @endforeach
         </table>
